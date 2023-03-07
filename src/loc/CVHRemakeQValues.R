73ac56c7b2948b5593f89df7eacefca893c07889
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-03-07 14:10:46 -0500

    Add same sign and all sign separation

--- src/loc/CVHRemakeQValues.R
+++ src/loc/CVHRemakeQValues.R
@@ -4,7 +4,124 @@ library(RERconverge)
 CVHCorrelationFile = readRDS("Data/CVHRemakeCorrelationFile.rds")
 CVHData = CVHCorrelationFile
 CVHData$permPValue = readRDS("Data/CVHRemakeCombinedPrunedFastAllPermulationsPValue.rds")
+CVHData$permPValueSameSign = readRDS("Data/CVHRemakeCombinedPrunedFastAllPermulationsPValue-SameSign.rds")
+CVHData$permPValueAllSign = readRDS("Data/CVHRemakeCombinedPrunedFastAllPermulationsPValue-AllSign.rds")
 
+
+str(CVHData$permPValue)
+
+CVHData[order(CVHData$permPValueSameSign),]
+CVHData[order(CVHData$p.adj),]
+
+range(CVHData$permPValueSameSign, na.rm = T)
+
+plot(CVHData$permPValueSameSign)
+
+
+# ---- Same sign version ---
+# --------------------------
+sameSignPermQ = qvalue(p = CVHData$permPValueSameSign)
+CVHData$sameSignPermQVal = sameSignPermQ$qvalues
+
+CVHDataPositive = CVHData[which(CVHData$Rho > 0),]
+CVHDataNegative = CVHData[which(CVHData$Rho < 0),]
+
+plot(CVHDataPositive$permPValueSameSign)
+plot(CVHDataNegative$permPValueSameSign)
+hist(CVHDataPositive$permPValueSameSign, breaks = 40, xaxp = c(0,1,20))
+hist(CVHDataNegative$permPValueSameSign, breaks = 40, xaxp = c(0,1,20))
+
+hist(CVHDataPositive$p.adj, breaks = 40, xaxp = c(0,1,20))
+hist(CVHDataNegative$p.adj, breaks = 40, xaxp = c(0,1,20))
+
+# -- order by q values 
+CVHData[order(CVHData$sameSignPermQ),]
+range(CVHData$simplePermQVal, na.rm = T)
+
+# -- plot q values -- 
+hist(CVHData$sameSignPermQ, breaks = 40, xaxp = c(0,1,20))
+
+hist(CVHData$sameSignPermQ, breaks = 40, xaxp = c(0,1,20), ylim = c(0,100))
+
+genesRankedPermP = rownames(CVHData[order(CVHData$permPValueSameSign),])
+write(genesRankedPermP, "Output/NewCVHRankedPermGenesSameSign.txt")
+
+# -- number of q values <0.05
+CVHDataVeryLowQ = CVHData[which(CVHData$sameSignPermQ < 0.05),]
+length(which(CVHData$sameSignPermQ < 0.05))
+
+
+# ---- All sign version ----
+# -------------------------
+
+# ---- Same sign version --- 
+allSignPermQ = qvalue(p = CVHData$permPValueAllSign)
+CVHData$permPValueAllSign = allSignPermQ$qvalues
+
+CVHDataPositive = CVHData[which(CVHData$Rho > 0),]
+CVHDataNegative = CVHData[which(CVHData$Rho < 0),]
+
+plot(CVHDataPositive$permPValueAllSign)
+plot(CVHDataNegative$permPValueAllSign)
+hist(CVHDataPositive$permPValueAllSign, breaks = 40, xaxp = c(0,1,20))
+hist(CVHDataNegative$permPValueAllSign, breaks = 40, xaxp = c(0,1,20))
+
+hist(CVHDataPositive$p.adj, breaks = 40, xaxp = c(0,1,20))
+hist(CVHDataNegative$p.adj, breaks = 40, xaxp = c(0,1,20))
+
+# -- order by q values 
+CVHData[order(CVHData$allSignPermQ),]
+range(CVHData$simplePermQVal, na.rm = T)
+
+# -- plot q values -- 
+hist(CVHData$allSignPermQ, breaks = 40, xaxp = c(0,1,20))
+
+hist(CVHData$allSignPermQ, breaks = 40, xaxp = c(0,1,20), ylim = c(0,100))
+
+genesRankedPermP = rownames(CVHData[order(CVHData$permPValueAllSign),])
+write(genesRankedPermP, "Output/NewCVHRankedPermGenesAllSign.txt")
+
+# -- number of q values <0.05
+CVHDataVeryLowQ = CVHData[which(CVHData$allSignPermQ < 0.05),]
+length(which(CVHData$allSignPermQ < 0.05))
+
+
+
+
+
+
+#plot RERs
+RERFile = readRDS("Data/CVHRemakeRERFile.rds")
+PathsFile = readRDS("Data/CVHRemakePathsFile.rds")
+plotRers(RERFile, "AC010255", PathsFile)
+for(i in 1:100){
+  plotRers(RERFile, genesRankedPermP[i], PathsFile)
+  message(i)
+  Sys.sleep(4)
+}
+#
+
+
+
+
+# -- plot the p and q values -- 
+par(mfrow = c(2,3))
+hist(carnvHerbsPreviousAnalysisData$permp.2sided, breaks = 40, xaxp = c(0,1,20))
+hist(newAnalysisData$permPValue, breaks = 40, xaxp = c(0,1,20))
+hist(newAnalysisData$sameSignPermPValue, breaks = 40, xaxp = c(0,1,20))
+
+hist(carnvHerbsPreviousAnalysisData$qvalue.2sided, breaks = 40, xaxp = c(0,1,20), ylim = c(0,100))
+hist(newAnalysisData$simplePermQVal, breaks = 40, xaxp = c(0,1,20), ylim = c(0,100))
+hist(newAnalysisData$sameSignQVal, breaks = 40, xaxp = c(0,1,20), ylim = c(0,100))
+
+length(newAnalysisData[which(newAnalysisData$sameSignQVal <0.05),])
+
+
+
+
+
+
+# --- Not remaned Version -----
 str(CVHData$permPValue)
 
 CVHData[order(CVHData$permPValue),]
