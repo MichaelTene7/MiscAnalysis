bba3a759223ee2b50e8e63009ac366f6275db92a
Michael Tene <110479175+MichaelTene7@users.noreply.github.com>
2024-09-19 19:20:46 -0400

    add toga tree

--- src/loc/scratchSapce.R
+++ src/loc/scratchSapce.R
@@ -19,4 +19,72 @@ names(uphamTips)[1] = "uphamName"
 write.csv(uphamTips, "Results/UphmanNameConversion.csv")
 
 
-nexusTree= read.nexus("Data/FoleyTree.txt")
+nexusTree= read.nexus("Data/FoleyTree.nex")
+
+obj<-scan(file="Data/FoleyTree.nex",n=1,skip=2,what="character")
+tree<-read.tree(text=obj)
+
+plotTree(tree)
+
+foleyTips = tree$tip.label
+write.csv(foleyTips, "Results/foleyTipList.csv")
+
+source("src/reu/ZoonomTreeNameToCommon.R")
+
+ZoonomTreeNameToCommon(tree, manualAnnotLocation = "Results/MergedData.csv", tipCol = "foleyTip")
+
+convertTree = ZoonomTreeNameToCommon(tree, manualAnnotLocation = "Results/MergedData.csv", tipCol = "foleyTip", commonCol = "Zoonomia")
+commonTree = ZoonomTreeNameToCommon(tree, manualAnnotLocation = "Results/MergedData.csv", tipCol = "foleyTip")
+
+
+
+convertTree$tip.label
+
+
+mergedData = read.csv("Results/MergedData.csv")
+
+
+
+zonomTipsMissing = mergedData$Zoonomia[which(is.na(mergedData$foleyTip))]
+
+zonomTipsMissing = zonomTipsMissing[which(!is.na(zonomTipsMissing))]
+
+missingCOmmons = mergedData$CommonName[which(mergedData$Zoonomia %in% zonomTipsMissing)]
+
+plotTreeHighlightBranches2(commonTree, hlspecies = missingCOmmons, hlcols = "blue")
+
+missingSpecies = mergedData$ScientificName[which(is.na(mergedData$foleyTip))]
+
+unique(mergedData$Scientific_Binomial)
+length(mergedData$Scientific_Binomial)
+mergedData$Scientific_Binomial[which(duplicated(mergedData$Scientific_Binomial))]
+
+togaTree = read.tree("Data/ZoonomiaTOGATree.txt")
+plotTreeHighlightBranches(togaTree, hlspecies = "REFRENCE")
+
+togaTips = togaTree$tip.label
+
+which(mergedData$TogaTreeName %in% togaTips)
+
+ZoonomiaNames = mergedData$Zoonomia
+ZoonomiaTrim = apply(matrix(ZoonomiaNames, nrow=1), 2, function(x) substr(x,4,nchar(x)))
+
+ZoonomiaTrim[which(!ZoonomiaTrim %in% togaTips)]
+
+mergedData = add_column(mergedData, TogaTreeName = ZoonomiaTrim, .before = "Divider")
+
+mergedData$TogaTreeName[which(!mergedData$TogaTreeName %in% togaTips)]
+
+togaTips
+
+newTogaTree = togaTree
+newTogaTree$tip.label = paste0("vs_", togaTree$tip.label)
+
+which(newTogaTree$tip.label == "vs_hg38")
+newTogaTree$tip.label[129] = "REFRENCE"
+
+write.tree(newTogaTree, "Output/TogaTree.nwk")
+
+test = read.tree("Output/TogaTree.nwk")
+
+is.binary(test)
